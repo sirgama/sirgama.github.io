c473a9a5e6a6e87a3b373578a317452a6e5f72ff
sirgama <sirengogamaliel@gmail.com>
2022-07-02 22:03:24 +0300

    update website link for thank you

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
 import { ThankyouComponent } from './thankyou/thankyou.component';
+import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,7 @@ import { ThankyouComponent } from './thankyou/thankyou.component';
     AppRoutingModule
 
   ],
-  providers: [],
+  providers: [Location, {provide: LocationStrategy, useClass: PathLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
